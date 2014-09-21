3ff123fe3532d3bee613a2d060605153792052a2
Nathan Fegard <nfegard@asu.edu>
2014-09-20 17:24:29 -0700

    Added webview to chat activity.

--- android/app/src/main/java/com/stevex86/miletus/Chat_Activity.java
+++ android/app/src/main/java/com/stevex86/miletus/Chat_Activity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
 public class Chat_Activity extends Activity {
@@ -12,6 +14,9 @@ public class Chat_Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 
 
