b47f28cb9bc68591af187044f5cb8357aeebcc81
Nathan Fegard <nfegard@asu.edu>
2014-09-20 21:26:14 -0700

    Fleshed out create user activity.

--- android/app/src/main/java/com/stevex86/miletus/Create_Account_Activity.java
+++ android/app/src/main/java/com/stevex86/miletus/Create_Account_Activity.java
@@ -4,14 +4,62 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Create_Account_Activity extends Activity {
 
+    private String username;
+    private String password;
+    private String confirmedPassword;
+    private String aKey;
+
+    EditText inputUsername;
+    EditText inputPassword;
+    EditText inputConfirmPassword;
+    EditText inputAuthenticationKey;
+    Button registerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create__account_);
+        setContentView(R.layout.activity_create_account);
+
+        registerButton = (Button)findViewById(R.id.register_button);
+        inputUsername = (EditText)findViewById(R.id.create_username);
+        inputPassword = (EditText)findViewById(R.id.create_password);
+        inputConfirmPassword = (EditText)findViewById(R.id.confirm_password);
+        inputAuthenticationKey = (EditText)findViewById(R.id.authentication_key);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean errors = false;
+                username = inputUsername.getText().toString();
+                aKey = inputAuthenticationKey.getText().toString();
+                password = inputPassword.getText().toString();
+                confirmedPassword = inputConfirmPassword.getText().toString();
+                if(!password.matches("[A-Za-z0-9]+"))
+                {
+                    errors = true;
+                }
+                if(!password.equals(confirmedPassword))
+                {
+                    errors = true;
+                }
+                if(!username.matches("[A-Za-z0-9]+"))
+                {
+                    errors = true;
+                }
+                if(!aKey.matches("[A-Za-z0-9\\-]"))
+                {
+                    errors = true;
+                }
+
+            }
+        });
     }
 
 
