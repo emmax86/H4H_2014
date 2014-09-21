34e2f0f76f621993288357f6dcf27e6cd9624c9a
root <root@stevex86.com>
2014-09-21 03:37:06 -0400

    Now runs on web server.

--- flask_chat/run.py
+++ flask_chat/run.py
@@ -8,5 +8,5 @@ monkey.patch_all()
 PORT = 5000
 
 if __name__ == '__main__':
-    print 'Listening on http://127.0.0.1:%s and on port 10843 (flash policy server)' % PORT
-    SocketIOServer(('', PORT), app, resource="socket.io").serve_forever()
+    print 'Listening on http://stevex86.com:%s and on port 10843 (flash policy server)' % PORT
+    SocketIOServer(('0.0.0.0', PORT), app, resource="socket.io").serve_forever()
